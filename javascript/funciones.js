3b1c85e9df026233cd0e144465be57ddd707699a
Josemaría García de Polavieja Ferre <jgf@altair.edu.es>
2024-10-14 13:45:59 +0200

    ejemplo corregido

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -1,10 +1,13 @@
-function creaImagen() {
+export function creaImagen() {
     // Función que crea un elemento imagen, completa sus clases y el src (puedes usar la imagen adjunta u otra)
-
+    const imagen = document.createElement('img');
+    imagen.className = "img-fluid";
+    imagen.src = "./imagenes/elcano.jpg";
     // Devuelve el elemento imagen
+    return imagen;
 }
 
-function creaTabla() {
+export function creaTabla() {
     const arrayHuracanes = [
         { cat: 1, vel: '120-153' },
         { cat: 2, vel: '154-177' },
@@ -13,13 +16,50 @@ function creaTabla() {
         { cat: 5, vel: 'Más de 250' },
     ]
     // Crea una tabla con los datos de este array
+    const table = document.createElement('table');
+    const tr = document.createElement('tr');
+    const th1 = document.createElement('th');
+    th1.innerText = "Categoría";
+    const th2 = document.createElement('th');
+    th2.innerText = "Velocidad";
+    tr.appendChild(th1);
+    tr.appendChild(th2);
+    table.appendChild(tr);
+    // Hasta aquí hemos creado la tabla y su fila de títulos
+    arrayHuracanes.forEach( item => {
+        const tr = document.createElement('tr');
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        td1.innerText = item.cat;
+        td2.innerText = item.vel;
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        table.appendChild(tr);
+    });
+    table.className = "table";
+    // Devuelvo la tabla formada
+    return table;
 }
 
-function creaParrafo() {
+export function creaParrafo() {
     // Crea un párrafo con texto genérico
+    const p = document.createElement('p');
+    p.innerText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae a maiores neque suscipit! Animi corrupti ut aspernatur nesciunt ipsam atque odio veniam vitae nemo! Totam maiores eos autem accusamus doloribus!";
+    return p;
 }
 
-function creaLista() {
+export function creaLista() {
     const lista = ['Bananas', 'Manzanas', 'Melocotones', 'Naranjas'];
     // Crea una lista con este array y la devuelve
+    const ul = document.createElement('ul');
+    ul.className="list-group";
+    // Por cada fruta en la lista, añadiré un elemento li a la lista ul
+    lista.forEach( fruta => {
+        const li = document.createElement('li');
+        li.innerText = fruta;
+        li.className = "list-group-item";
+        ul.appendChild(li);
+    } );
+    // Devuelvo la lista
+    return ul;
 }
--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,7 @@
 // Añadir las importaciones de las funciones que crean la imagen, la tabla, el párrafo y l lista
 
+import { creaImagen, creaLista, creaParrafo, creaTabla } from "./funciones.js";
+
 // Variables para guardar la zona y la entrada
 let zona = '';
 let entrada = '';
@@ -7,16 +9,22 @@ let entrada = '';
 // Esta función guarda la zona seleccionada
 function seleZona() {
     // Selecciona la zona del select elegir zona y lo guarda
-    
+    zona = document.querySelector('#zona').value;
     // Si hay entrada, habilito el botón aplicar
+    if (entrada != '') {
+        document.querySelector('#aplicar').disabled = false;
+    }
     
 }
 
 // Esta función guarda la entrada seleccionada
 function seleInput() {
     // Selecciona la entrada del select elegir entrada y la guarda
-    
+    entrada = document.querySelector('#entrada').value;
     // Si hay zona elegida, hablitar el botón aplicar
+    if (zona != '') {
+        document.querySelector('#aplicar').disabled = false;
+    }
     
 }
 
@@ -24,10 +32,35 @@ function seleInput() {
 la entrada seleccionada y lo coloca en la zona seleccionada. */
 function enviar() {
     // Limpiar la zona seleccionada (para que no se coloquen más de una entrada en la zona)
-    
+    document.querySelector(`#${zona}`).innerHTML = "";
     /* comprueba cual es la zona seleccionada y ejecuta la función de creación
     correspondiente (switch)*/
+    switch (entrada) {
+        case 'p':
+            document.querySelector(`#${zona}`).append(creaParrafo());
+            break;
+        case 'img':
+            document.querySelector(`#${zona}`).append(creaImagen());
+            break;
+        case 'table':
+            document.querySelector(`#${zona}`).append(creaTabla());
+            break;
+        case 'list':
+            document.querySelector(`#${zona}`).append(creaLista());
+            break;
+    }
     
 }
 
+function limpiar() {
+    document.querySelector('#A').innerHTML = "";
+    document.querySelector('#B').innerHTML = "";
+    document.querySelector('#C').innerHTML = "";
+    document.querySelector('#D').innerHTML = "";
+}
+
 // Asignación de funciones al objeto window
+window.enviar = enviar;
+window.seleZona = seleZona;
+window.seleInput = seleInput;
+window.limpiar = limpiar;
